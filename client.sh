f6cc79b012ff08518e32519e8317e37f1782403b
Qingyang Long <qylong@bu.edu>
2020-12-06 01:46:06 -0500

    ssh command

--- client.sh
+++ client.sh
@@ -8,6 +8,7 @@ sudo apt install python3-pip
 sudo python3.7 -m pip install pip
 sudo python3.7 -m pip install opencv-python==4.4.0.46
 sudo python3.7 -m pip install numpy==1.19.4
+sudo python3.7 -m pip install flask
 sudo mkdir -p /users/geni
 cd /users/geni
 sudu git clone https://github.com/LyapunovJingci/CS655_Project.git
--- server.sh
+++ server.sh
@@ -9,6 +9,8 @@ sudo python3.7 -m pip install pip
 sudo python3.7 -m pip install opencv-python==4.4.0.46
 sudo python3.7 -m pip install numpy==1.19.4
 sudo python3.7 -m pip install imageai==2.1.5
+sudo python3.7 -m pip install cmake
+sudo python3.7 -m pip install dlib
 sudo python3.7 -m pip install face_recognition
 sudo mkdir -p /users/geni
 cd /users/geni
