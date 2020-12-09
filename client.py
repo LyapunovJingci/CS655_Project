209f85de34ab024f47131985d6bd5a66ae8d2c0e
Qingyang Long <781706860@qq.com>
2020-12-09 17:44:31 -0500

    Update client.py

--- client.py
+++ client.py
@@ -25,86 +25,70 @@ def socket_client():
     except socket.error as msg:
         print(msg)
         sys.exit(1)
+    count = 0
+    correct = 0
 
-    linkStart = time.time();
-    while 1:
-        # send first picture
-        filepath = os.path.join(os.getcwd() + '/image/obama.jpg')
-        print(filepath)
-        if os.path.isfile(filepath):
-            size1 = os.path.getsize(filepath)
-            print(size_format(size1))
+    while(count < 100):
+        linkStart = time.time();
+        while 1:
+            # send first picture
+            filepath = os.path.join(os.getcwd() + '/image/obama.jpg')
+            print(filepath)
+            if os.path.isfile(filepath):
+                size1 = os.path.getsize(filepath)
+                print(size_format(size1))
+                fileinfo_size = struct.calcsize('128sq')
+                fhead = struct.pack('128sq', bytes(os.path.basename(filepath).encode('utf-8')),
+                                    os.stat(filepath).st_size)
+                s.send(fhead)
+                # print('client filepath: {0}'.format(filepath))
+                fp = open(filepath, 'rb')
+                while 1:
+                    data = fp.read(1024)
+                    if not data:
+                        print('{0} file send over...'.format(filepath))
+                        break
+                    s.send(data)
+
+            firstFileTime = time.time()
+            # send second picture
+            filepath2 = os.path.join(os.getcwd() + '/image/obama2.jpg')
+            if os.path.isfile(filepath2):
+                fileinfo_size2 = struct.calcsize('128sq')
+                size2 = os.path.getsize(filepath2)
+                print(size_format(size2))
+                fhead = struct.pack('128sq', bytes(os.path.basename(filepath2).encode('utf-8')),
+                                    os.stat(filepath2).st_size)
+                s.send(fhead)
+                # print('client filepath: {0}'.format(filepath))
+                fp = open(filepath2, 'rb')
+                while 1:
+                    data = fp.read(1024)
+                    if not data:
+                        print('{0} file send over...'.format(filepath2))
+                        break
+                    s.send(data)
+
+            secondFileTime = time.time()
+            # get result
             fileinfo_size = struct.calcsize('128sq')
-            fhead = struct.pack('128sq', bytes(os.path.basename(filepath).encode('utf-8')), os.stat(filepath).st_size)
-            s.send(fhead)
-            #print('client filepath: {0}'.format(filepath))
-            fp = open(filepath, 'rb')
-            while 1:
-                data = fp.read(1024)
-                if not data:
-                    print('{0} file send over...'.format(filepath))
-                    break
-                s.send(data)
-
-        firstFileTime = time.time()
-        # send second picture
-        filepath2 = os.path.join(os.getcwd() + '/image/obama2.jpg')
-        if os.path.isfile(filepath2):
-            fileinfo_size2 = struct.calcsize('128sq')
-            size2 = os.path.getsize(filepath2)
-            print(size_format(size2))
-            fhead = struct.pack('128sq', bytes(os.path.basename(filepath2).encode('utf-8')), os.stat(filepath2).st_size)
-            s.send(fhead)
-            #print('client filepath: {0}'.format(filepath))
-            fp = open(filepath2, 'rb')
-            while 1:
-                data = fp.read(1024)
-                if not data:
-                    print('{0} file send over...'.format(filepath2))
-                    break
-                s.send(data)
-
-        secondFileTime = time.time()
-        # get result
-        fileinfo_size = struct.calcsize('128sq')
-        msg = s.recv(fileinfo_size)
-        result = msg.decode("utf-8")
-        print(result)
-        if(result == "same people!"):
-            print("""\
-                <html>
-                <head>
-                <meta charset="utf-8">
-                <title>CS655 geni project</title>
-                </head>
-                <body>
-                <h1>Same people</h1>
-                <a href="/index.html">
-                    <button>try again!</button>
-                </a>
-                </body>
-                </html>
-                """)
-        else:
-            print("""\
-                <html>
-                <head>
-                <meta charset="utf-8">
-                <title>CS655 geni project</title>
-                </head>
-                <body>
-                <h1>not same people</h1>
-                <a href="/index.html">
-                    <button>try again!</button>
-                </a>
-                </body>
-                </html>
-                """)
-        print('first picture takes:',firstFileTime-linkStart)
-        print('second picture takes:', secondFileTime - firstFileTime)
-        print('total takes:', secondFileTime - linkStart)
-        s.close()
-        break
-
+            msg = s.recv(fileinfo_size)
+            result = msg.decode("utf-8")
+            print(result)
+            if (result == "same people!"):
+                print("same people!")
+                correct += 1
+                count += 1
+            else:
+                print("not same:(")
+                count += 1
+            print('first picture takes:', firstFileTime - linkStart)
+            print('second picture takes:', secondFileTime - firstFileTime)
+            print('total takes:', secondFileTime - linkStart)
+            
+    s.close()
+    print('sent:',count)
+    print('correct:',correct)
+        
 if __name__ == '__main__':
     socket_client()
